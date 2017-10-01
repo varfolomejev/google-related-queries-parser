9748e946338ede22ac9066ac47ff4abe7d459135
igor <varfolomejevi@gmail.com>
2017-10-01 17:05:02 +0300

    base version

---
+++ google-related-queries-parser.js
@@ -0,0 +1,60 @@
+const url = require('url');
+const nightmare = require('nightmare')({ show: true });
+const cheerio = require('cheerio');
+
+class GoogleRelatedQueriesParser
+{
+    constructor(settings){
+        this.protocol = settings.protocol || 'https:';
+        this.hostname = settings.hostname || 'www.google.com';
+        this.pathname = settings.pathname || '/search';
+        this.relatedQueriesBockId = settings.relatedQueriesBockId || 'brs';
+    }
+
+    async parse(searchString, callback){
+        let queries = [];
+        nightmare
+            .goto(this.getUrl(searchString))
+            .wait('#' + this.relatedQueriesBockId)
+            .evaluate(function () {
+                let result = null;
+                if(document.querySelector('#brs')){
+                    result = document.querySelector('#brs').innerHTML;
+                }
+                return result;
+            })
+            .end()
+            .then(function (html) {
+                if(html){
+                    let $ = cheerio.load(html);
+                    $('._e4b').each(function(i, elem) {
+                        queries.push({
+                            title: $(this).text(),
+                            url: $(this).find('a').attr('href'),
+                            html: $(this).find('a').html(),
+                        });
+                    });
+                }
+                callback(null, queries);
+            })
+            .catch(function (error) {
+                callback(error);
+            });
+    }
+
+    getUrl(searchString){
+        let urlObj = {
+            protocol: this.protocol,
+            hostname: this.hostname,
+            pathname: this.pathname,
+            query: {
+                q: searchString,
+                sourceid: 'chrome',
+                ie: 'UTF-8'
+            },
+        };
+        return url.format(urlObj)
+    }
+}
+
+module.exports = GoogleRelatedQueriesParser;
